8307694181525e1b1e7604a1914e624b9107f9a5
abdelhalim <rioman19@gmail.com>
2024-02-27 09:30:29 -0500

    feat: choose diff dataset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { csv, type DSVRowArray } from 'd3';
+	import { csv, csvParse, type DSVRowArray } from 'd3';
 	import LineChart from '$lib/LineChart.svelte';
 	import { onMount } from 'svelte';
 
@@ -21,13 +21,48 @@
 		}));
 	};
 
+	let reader: FileReader;
+
 	onMount(() => {
+		reader = new FileReader();
+
+		const onload = (ev: ProgressEvent<FileReader>) => {
+			console.log(ev.target?.result);
+			const raw = ev.target?.result;
+
+			const d = csvParse(raw);
+			data = dataParser(d);
+		};
+
+		reader.addEventListener('load', onload);
+
 		csv('/data/data.csv').then((d) => {
 			data = dataParser(d);
 		});
+
+		return () => {
+			reader.removeEventListener('load', onload);
+		};
 	});
+
+	function onchange(ev: Event) {
+		const currentTarget = ev.currentTarget as HTMLInputElement;
+
+		const files = currentTarget.files;
+		const first_line = files?.item(0);
+
+		if (first_line) {
+			reader.readAsText(first_line);
+		}
+
+		console.log(files);
+	}
 </script>
 
-<div class="h-[100svh] w-[100svw]">
+<div class="h-[100svh] w-[100svw] relative">
 	<LineChart {data} {groupByAccessor} {xAccessor} {yAccessor} {lclAccessor} {uclAccessor} />
+
+	<div class="absolute bottom-0 right-0 p-2 z-[3]">
+		<input type="file" on:change={onchange} />
+	</div>
 </div>
